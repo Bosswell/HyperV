c168f853c5b7849d52db938c59b057cdb00c621e
Jakub Batko <bosswell9@gmail.com>
2019-11-05 21:43:26 +0100

    Save crawler facade prototype

--- src/Facade/WebsiteCrawlerFacade.php
+++ src/Facade/WebsiteCrawlerFacade.php
@@ -4,35 +4,57 @@ namespace App\Facade;
 
 use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class WebsiteCrawlerFacade
 {
     private $websiteLink = 'https://symfony.com/doc/current/components/dom_crawler.html';
-    private $testCase = [
-        'XPath' => [
-            'Setup' => '//html/body/div[2]/div/div[2]',
+    private $selectors = [
+        'Setup' => [
+            'path' => '//html/body/div[2]/div/div[2]',
+            'type' => 'XPath'
         ],
-        'CSSSelector' => [
-            'Installation' => '#installation > h2:nth-child(1)'
-        ]
+        'Installation' => [
+            'path' => '//html/body/div[2]/div/div[2]',
+            'type' => 'CSSSelector'
+        ],
     ];
 
     /** @var Crawler */
     private $crawler;
 
+    /** @var HttpClient */
+    private $httpClient;
+
     public function __construct()
     {
-        $client = HttpClient::create();
-        $response = $client->request('GET', $this->websiteLink);
-        $content = $response->getContent();
-        $this->crawler = new Crawler($content);
-        $xd = '//main/h1';
-        dump($this->crawler->filterXPath($this->testCase['XPath']['Setup'])->filterXPath($xd)->text());
-//        dump($this->crawler->filter($this->testCase['CSSSelector']['Installation']));
+        $this->httpClient = HttpClient::create();
+        $this->crawler = new Crawler();
     }
 
-    public function crawl()
+    /**
+     * @throws TransportExceptionInterface
+     */
+    private function crawlWebsite(string $pageLink)
     {
-//        $this->crawler->
+        $this->crawler->add(
+            $this->httpClient->request('GET', $pageLink)->getContent()
+        );
+
+        foreach ($this->selectors as $selector) {
+            switch ($selector['type']) {
+                case 'XPath':
+                    $this->crawler->filterXPath($selector['path']);
+                    break;
+                case 'CSSSelector':
+                    $this->crawler->filter($selector['path']);
+                    break;
+                default:// wyjątek może być  wyrzucowny po zdefiniowaniu klasy selector
+                   throw new \Exception(sprintf(
+                       'Invalid selector type. Allowed [XPath ,CSSSelector], given [%s]',
+                       $selector['type']
+                   ));
+            }
+        }
     }
 }
