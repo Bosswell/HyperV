0decf30e162faeeb97eff2cd1c1b19880154875c
Jakub Batko <bosswell9@gmail.com>
2019-11-30 23:41:31 +0100

    Configured monolog handler, added console command for crawling

---
+++ src/Command/WebCrawlerCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Command;
+
+
+use App\Dto\Crawler\CrawlerGetLinks;
+use App\Exception\ValidationException;
+use App\PageExtractor\ExtractorException;
+use App\PageExtractor\LinkExtractorFacade;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class WebCrawlerCommand extends Command
+{
+    /** @var LinkExtractorFacade */
+    private $linkExtractorFacade;
+
+    public function __construct(LinkExtractorFacade $linkExtractorFacade)
+    {
+        parent::__construct('crawler:extract:links');
+
+        $this->linkExtractorFacade = $linkExtractorFacade;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->addOption('domainUrl', 'd', InputOption::VALUE_REQUIRED, 'Website base domain url ex. http://youtube.com/')
+            ->addOption('pattern', 'p',InputOption::VALUE_OPTIONAL, 'RageXp for URLs ex. /d+-.*')
+            ->addOption('excludedPaths', 'exPaths',InputOption::VALUE_OPTIONAL | InputOption::VALUE_IS_ARRAY, 'Excluded paths ex. /pl/, /de/, ?search')
+        ;
+    }
+
+    /**
+     * @return void
+     * @throws ValidationException
+     * @throws ExtractorException
+     * @param OutputInterface $output
+     * @param InputInterface $input
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $crawlerGetLinks = new CrawlerGetLinks($input->getOptions());
+
+        $output->writeln('Extracting domain urls..');
+        $output->writeln('Visit /var/log/linkCrawler.log to see more details');
+        $this->linkExtractorFacade->getLinks($crawlerGetLinks);
+    }
+}
